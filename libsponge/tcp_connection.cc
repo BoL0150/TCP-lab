af4e7b41ccea5f61a8496dc9b898ea12a5d18096
BoLee <1160360517@qq.com>
2021-09-26 17:36:37 +0800

    lab4

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -12,17 +12,31 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-size_t TCPConnection::remaining_outbound_capacity() const { return {}; }
+size_t TCPConnection::remaining_outbound_capacity() const { return _sender.stream_in().remaining_capacity(); }
 
-size_t TCPConnection::bytes_in_flight() const { return {}; }
+size_t TCPConnection::bytes_in_flight() const { return _sender.bytes_in_flight(); }
 
-size_t TCPConnection::unassembled_bytes() const { return {}; }
+size_t TCPConnection::unassembled_bytes() const { return _receiver.unassembled_bytes(); }
 
 size_t TCPConnection::time_since_last_segment_received() const { return {}; }
 
-void TCPConnection::segment_received(const TCPSegment &seg) { DUMMY_CODE(seg); }
+void TCPConnection::segment_received(const TCPSegment &seg) { 
+    TCPHeader header = seg.header();
+    // 如果设置了RST，直接中断连接
+    if(header.rst){
+        _receiver.stream_out()._error = false;
+        _sender.stream_in()._error = false;
+        _alive = false;
+        return;
+    }
+    _receiver.segment_received(seg);
+    // 如果设置了ack标志位，则将ackno和window size传入TCPSender
+    if(header.ack){
+        _sender.ack_received(header.ackno,header.win);
+    }
+}
 
-bool TCPConnection::active() const { return {}; }
+bool TCPConnection::active() const { return _alive;}
 
 size_t TCPConnection::write(const string &data) {
     DUMMY_CODE(data);
@@ -34,7 +48,9 @@ void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_
 
 void TCPConnection::end_input_stream() {}
 
-void TCPConnection::connect() {}
+void TCPConnection::connect() {
+    _sender.fill_window();
+}
 
 TCPConnection::~TCPConnection() {
     try {
--- libsponge/tcp_connection.hh
+++ libsponge/tcp_connection.hh
@@ -10,38 +10,45 @@
 class TCPConnection {
   private:
     TCPConfig _cfg;
+    // 初始化TCPReceiver
     TCPReceiver _receiver{_cfg.recv_capacity};
+    // 初始化TCPSender
     TCPSender _sender{_cfg.send_capacity, _cfg.rt_timeout, _cfg.fixed_isn};
-
-    //! outbound queue of segments that the TCPConnection wants sent
+    // TCPConnection准备发送的TCPsegment的输出队列
+    // TCPSender中 _segments_out 中的segment不包括ackno和window size，不是TCPConnection真正发送出去的TCPsegment,
+    // 需要与TCPReceiver的ackno()和window_size()的返回值结合在一起，再放入到TCPConnection 中的 _segments_out 中。
+    // 我们可以认为放入此队列的segment已经发送出去了，但是实际上需要由拥有者或OS将该队列中的segment pop出，然后一一
+    // 封装进更底层的IP数据报或UDP，此时才真正发送出去了
     std::queue<TCPSegment> _segments_out{};
 
-    //! Should the TCPConnection stay active (and keep ACKing)
-    //! for 10 * _cfg.rt_timeout milliseconds after both streams have ended,
-    //! in case the remote TCPConnection doesn't know we've received its whole stream?
+    // TCPConnection是否alive
+    bool _alive{true};
+    // TCPConnection是否应该在两个流结束后保持alive 10 * _cfg.rt_timeout的时间
     bool _linger_after_streams_finish{true};
 
   public:
-    //! \name "Input" interface for the writer
-    //!@{
+    //! 提供给应用层writer的输入接口
 
-    //! \brief Initiate a connection by sending a SYN segment
+    // 通过发送SYN segment 初始化连接
     void connect();
 
-    //! \brief Write data to the outbound byte stream, and send it over TCP if possible
-    //! \returns the number of bytes from `data` that were actually written.
+    // 应用层向输出流写入数据，如果有可能的话通过TCP发送
+    // 返回实际写入的数据的字节数
     size_t write(const std::string &data);
 
+    // 返回应用层可以向ByteStream写入的字节数,即ByteStream的空闲空间大小
     //! \returns the number of `bytes` that can be written right now.
     size_t remaining_outbound_capacity() const;
 
+    // 结束向TCPConnection中写入，也就是关闭输出流（仍然允许读取输入的数据）
     //! \brief Shut down the outbound byte stream (still allows reading incoming data)
     void end_input_stream();
-    //!@}
 
+    // 提供给应用层reader的输出接口
     //! \name "Output" interface for the reader
     //!@{
 
+    // 已经从对等端收到的输入字节流
     //! \brief The inbound byte stream received from the peer
     ByteStream &inbound_stream() { return _receiver.stream_out(); }
     //!@}
@@ -59,35 +66,36 @@ class TCPConnection {
     TCPState state() const { return {_sender, _receiver, active(), _linger_after_streams_finish}; };
     //!@}
 
+    // 由拥有者或操作系统调用的方法
     //! \name Methods for the owner or operating system to call
     //!@{
 
+    // 当从网络中接收到一个segment时调用
     //! Called when a new segment has been received from the network
     void segment_received(const TCPSegment &seg);
 
+    // 当时间流逝时，周期性调用
     //! Called periodically when time elapses
     void tick(const size_t ms_since_last_tick);
 
-    //! \brief TCPSegments that the TCPConnection has enqueued for transmission.
-    //! \note The owner or operating system will dequeue these and
-    //! put each one into the payload of a lower-layer datagram (usually Internet datagrams (IP),
-    //! but could also be user datagrams (UDP) or any other kind).
+    // 返回TCPConnection想要输出的TCPSegment队列。
+    // 拥有者或OS会将此队列中的segment全部出队, 然后将每一个TCPSegment
+    // 放入低层次的数据报中（通常是IP数据包，也可以是UDP）,并真正地发送出去
     std::queue<TCPSegment> &segments_out() { return _segments_out; }
 
-    //! \brief Is the connection still alive in any way?
-    //! \returns `true` if either stream is still running or if the TCPConnection is lingering
-    //! after both streams have finished (e.g. to ACK retransmissions from the peer)
+    // 连接是否alive,返回true如果流还在运行，或者流结束后TCPConnection在linger
     bool active() const;
     //!@}
 
-    //! Construct a new connection from a configuration
+    // 从_cfg配置构造一个TCPConnection
     explicit TCPConnection(const TCPConfig &cfg) : _cfg{cfg} {}
 
-    //! \name construction and destruction
-    //! moving is allowed; copying is disallowed; default construction not possible
+    //! \name 构造函数和析构函数
+    // 允许移动，不允许拷贝，不允许默认构造
 
     //!@{
-    ~TCPConnection();  //!< destructor sends a RST if the connection is still open
+    // 如果连接还存活析构函数就发送一个RST
+    ~TCPConnection();  
     TCPConnection() = delete;
     TCPConnection(TCPConnection &&other) = default;
     TCPConnection &operator=(TCPConnection &&other) = default;
